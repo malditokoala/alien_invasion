ead144ac1ccba3c4ff51538c5e73b2a4a6c91e40
Edgar Gomez Borboa <edgargomezborboa@hotmail.com>
2019-09-15 22:59:56 -0700

    Update

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
 
         # Bullets Settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3
+        self.bullet_width = 3.5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
